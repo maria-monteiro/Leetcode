f71ff58be670ff880f3bcc98e20ca0b7b9fd1564
maria-monteiro <maria.camara@aluno.ifsp.edu.br>
2024-02-26 15:12:10 -0300

    feat: add comment to exercise

--- contains-duplicate.py
+++ contains-duplicate.py
@@ -1,10 +1,10 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
 def containsDuplicate(nums):
     nums.sort()
-
     for i in range(len(nums) - 1):
         if nums[i] == nums[i + 1]:
             return True
-
     return False
     
 
